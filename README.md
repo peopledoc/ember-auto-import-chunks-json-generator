3b1356ac5a796c73bca60ee299a517045742c7a5
Aleksandr Kanunnikov <aleksandr.kanunnikov@ukg.com>
2021-12-02 14:56:25 +0300

    hello world

---
+++ README.md
@@ -0,0 +1,37 @@
+ember-auto-import-chunks-json-generator
+==============================================================================
+
+Generates `chunks.json` file in dist folder, with list of chunks, used by `ember-auto-import` for `index.html` assets.
+
+
+Compatibility
+------------------------------------------------------------------------------
+
+* Ember.js v3.20 or above
+* Ember CLI v3.20 or above
+* Node.js v12 or above
+
+
+Installation
+------------------------------------------------------------------------------
+
+```
+ember install ember-auto-import-chunks-json-generator
+```
+
+
+Usage
+------------------------------------------------------------------------------
+
+Install addon, ensure you have `ember-auto-import 2` dependency on your application.
+
+Contributing
+------------------------------------------------------------------------------
+
+See the [Contributing](CONTRIBUTING.md) guide for details.
+
+
+License
+------------------------------------------------------------------------------
+
+This project is licensed under the [MIT License](LICENSE.md).
---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// eslint-disable-next-line node/no-extraneous-require
+const { MergeTrees } = require('broccoli-merge-trees');
+// eslint-disable-next-line node/no-extraneous-require
+const Plugin = require('broccoli-plugin');
+const fs = require('fs');
+const path = require('path');
+
+class ChunksWriter extends Plugin {
+  constructor(inputNodes, options = {}) {
+    super(inputNodes, {
+      annotation: options.annotation,
+      // see `options` in the below README to see a full list of constructor options
+    });
+    this.inserter = options.inserter;
+  }
+
+  build() {
+    const content = JSON.stringify(this.inserter.categorizeChunks(), null, 2);
+    fs.writeFileSync(
+      path.join(this.outputPath, 'chunks.json'),
+      content,
+      'utf8'
+    );
+  }
+}
+
+module.exports = {
+  name: require('./package').name,
+
+  isDevelopingAddon() {
+    return true;
+  },
+
+  postprocessTree(which, tree) {
+    if (which === 'all') {
+      if (!tree.inputNodes) {
+        throw new Error(
+          'Unknown build tree configuration, this addon should follow exactly after ember-auto-import'
+        );
+      }
+      const inserter = tree.inputNodes.find(
+        (node) => node._name === 'Inserter'
+      );
+      const chunkWriterInstance = new ChunksWriter(['app'], { inserter });
+      return new MergeTrees([tree, chunkWriterInstance]);
+    }
+    return tree;
+  },
+};
